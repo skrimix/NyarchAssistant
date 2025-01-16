54a5a1f8ed68ee230867ee31b6891fd5de886d11
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-01-16 17:02:33 +0100

    divide translate module

--- src/handlers/translator/__init__.py
+++ src/handlers/translator/__init__.py
@@ -1,1 +1,13 @@
-from .translator import *
+from .translator import TranslatorHandler
+from .googletr_handler import GoogleTranslatorHandler
+from .customtr_handler import CustomTranslatorHandler
+from .libretranslate_handler import LibreTranslateHandler
+from .ligva_handler import LigvaTranslateHandler
+
+__all__ = [
+    "TranslatorHandler",
+    "GoogleTranslatorHandler",
+    "CustomTranslatorHandler",
+    "LibreTranslateHandler",
+    "LigvaTranslateHandler",
+]
